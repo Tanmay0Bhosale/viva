034035ad929453f16a8ae4bd6098513c3b24e422
Tanmay <tanmaytanvi1234@gmail.com>
2025-12-18 12:34:32 +0530

    added a console app

---
+++ voting/voting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote.");
+
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote.");
+            }
+        }
+    }
+}
